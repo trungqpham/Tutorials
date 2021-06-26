954a8c1ccf17735842a0eb60de8dc34092727900
Trung Pham <55195578+trungqpham@users.noreply.github.com>
2021-06-26 09:35:47 -0700

    MvcMovie initial commit
    ASP.NET Core MVC tutorial projec

---
+++ MvcMovie/MvcMovie/Controllers/HellowWorldController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            // NOTE: ViewData dictionary object contains data that will be
+            // passed to the view
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
